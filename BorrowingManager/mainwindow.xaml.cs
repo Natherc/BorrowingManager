94a840d5e2f4f8dbf127166ad8670e4ee21b42e3
Nathan Dolinski <nathan@dado.be>
2017-08-02 13:19:19 +0200

    Binded List Territories to DataGrid

--- BorrowingManager/mainwindow.xaml.cs
+++ BorrowingManager/mainwindow.xaml.cs
@@ -29,7 +29,16 @@ namespace BorrowingManager
             con.ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = \"D:\\Documents\\Visual Studio 2015\\Projects\\BorrowingManager\\BorrowingManager\\borrowingManagerDB.mdf\"; Integrated Security = True";
             con.Open();
             connectLabel.Content = "Ouverture de Connexion";
+            List<Territory> listTerritory = new List<Territory>();
+            DateTime dt = new DateTime(2013,12,1);
+            listTerritory.Add(new Territory("A1", "Pecrot", DateTime.Now));
+            listTerritory.Add(new Territory("A2", "Grez", dt));
             
+            dataTerritory.ItemsSource = listTerritory.OrderBy(x => x.Date);
+
+
+
+
         }
     }
 }
